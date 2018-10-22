b73fe7b92a170c6fb63aea0cc95927f7f5be8800
Sara Frandsen <sara.frandsen@gmail.com>
2018-10-21 21:15:21 -0700

    Add beginning props to Example

--- src/components/example/Example.js
+++ src/components/example/Example.js
@@ -1,9 +1,25 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import styles from './Example.css'
 
 class Example extends Component {
-    render() {
-      return
-    }
+  render() {
+    const {
+      title,
+      description,
+      url
+    } = this.props;
+
+    return (
+      <section />
+    )
+  }
+}
+
+Example.PropTypes = {
+  title: PropTypes.string,
+  description: PropTypes.arrayOf(PropTypes.string),
+  url: PropTypes.string
 }
 
 export default Example;
