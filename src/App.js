d26ab0fe195a617e77c4f3556ec03fab571a8313
Frandsen, Sara <sara.frandsen@nordstrom.com>
2018-11-02 16:40:51 -0700

    Create homepage component to hold CV

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {
     Route
 } from "react-router-dom";
 import Intro from './components/intro/Intro';
+import HomePage from './components/homePage/HomePage';
 import ProfessionalPage from './components/professionalPage/ProfessionalPage';
 
 class App extends Component {
@@ -10,6 +11,7 @@ class App extends Component {
         return (
             <main>
                 <Route exact path="/" component={Intro} />
+                <Route path="/home" component={HomePage} />
                 <Route path="/professional" component={ProfessionalPage} />
             </main>
         );
